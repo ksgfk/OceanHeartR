9e42ed86258573b1be40af965abe3fa7f25d3387
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-08-16 12:10:28 +0800

    feat:对应的锭

--- src/main/java/com/github/ksgfk/oceanheartr/common/init/OHRItems.java
+++ src/main/java/com/github/ksgfk/oceanheartr/common/init/OHRItems.java
@@ -11,6 +11,12 @@ import net.minecraft.item.Item;
 @ModRegistry
 public final class OHRItems {
     @OreDict(name = "ingotOceanSoul")
-    public static final Item OceanSoul = new ItemOHRBase("ocean_soul_ingot");
+    public static final Item OceanSoulIngot = new ItemOHRBase("ocean_soul_ingot");
+    @OreDict(name = "ingotLavaSoul")
+    public static final Item LavaSoulIngot = new ItemOHRBase("lava_soul_ingot");
+    @OreDict(name = "ingotLegend")
+    public static final Item LegendIngot = new ItemOHRBase("legend_ingot");
+    @OreDict(name = "ingotVoid")
+    public static final Item VoidIngot = new ItemOHRBase("void_ingot");
     public static final Item FrigidDrillingBitRtx3080 = new ItemOHRBase("frigid_drilling_bit_rtx3080");
 }
